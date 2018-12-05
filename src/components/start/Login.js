ea1de533182adefebc1936f1f749ef2dca32e600
Elyse Dawson <dawson.elyse@gmail.com>
2018-12-05 11:41:34 -0600

    Register validates and adds new user to database. Login validates and adds user to session storage

--- src/components/start/Login.js
+++ src/components/start/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import validate from '../../modules/User/Validate'
 
 export default class Login extends Component {
   state = {
@@ -16,19 +17,39 @@ export default class Login extends Component {
 
   LoginForm = () => {
     return (
-      <Form>
+      <Form onSubmit={(e) => this.submitLogin(e)}>
         <FormGroup>
           <Label for="exampleEmail"><i className="fa fa-envelope form-icon" aria-hidden="true"></i>Email</Label>
-          <Input type="email" name="email" id="Email" placeholder="E-mail" />
+          <Input type="email" name="email" id="email" placeholder="E-mail" required onChange={this.handleFieldChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="examplePassword"><i className="fa fa-key form-icon" aria-hidden="true"></i>
             Password</Label>
-          <Input type="password" name="password" id="Password" placeholder="Password" />
+          <Input type="password" name="password" id="password" placeholder="Password" required onChange={this.handleFieldChange}/>
         </FormGroup>
-        <Button><i className="fa fa-arrow-circle-right form-icon" aria-hidden="true"></i></Button>
+        <Button type="submit"><i className="fa fa-arrow-circle-right form-icon" aria-hidden="true"></i></Button>
         <Button onClick={() => this.show()}><i className="fa fa-times-circle form-icon" aria-hidden="true"></i></Button>
       </Form>)
+  }
+
+  // Function that updates state to reflect user input
+  handleFieldChange = e => {
+    const stateToChange = {}
+    stateToChange[e.target.id] = e.target.value
+    this.setState(stateToChange)
+  }
+
+  submitLogin = (e) => {
+    e.preventDefault()
+    let obj = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(obj)
+    //validate and submit
+    validate.existingUser(obj)
+    console.log("Login Successful")
+    this.show()
   }
 
   render() {
--- src/components/start/Register.js
+++ src/components/start/Register.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import validate from '../../modules/User/Validate'
 
 export default class Register extends Component {
   state = {
     form: false,
     email: "",
-    password: ""
+    password: "",
+    firstName: ""
   }
 
   show = () => {
@@ -16,29 +18,51 @@ export default class Register extends Component {
 
   RegisterForm = () => {
     return (
-      <Form>
+      <Form onSubmit={(e) => this.submitRegistration(e)}>
         <FormGroup>
           <Label for="Email"><i className="fa fa-envelope form-icon" aria-hidden="true"></i>Email</Label>
-          <Input type="email" name="email" id="Email" placeholder="E-mail" />
+          <Input type="email" name="email" id="email" placeholder="E-mail" required onChange={this.handleFieldChange} />
         </FormGroup>
         <FormGroup>
           <Label for="Password"><i className="fa fa-key form-icon" aria-hidden="true"></i>
             Password</Label>
-          <Input type="password" name="password" id="Password" placeholder="Password" />
+          <Input type="password" name="password" id="password" placeholder="Password" required onChange={this.handleFieldChange} />
         </FormGroup>
         <FormGroup>
           <Label for="FirstName"><i className="fa fa-user-circle form-icon" aria-hidden="true"></i>First Name</Label>
-          <Input type="text" name="firstname" id="FirstName" placeholder="First Name" />
+          <Input type="text" name="firstname" id="firstName" placeholder="First Name" required onChange={this.handleFieldChange} />
         </FormGroup>
-        <Button><i className="fa fa-arrow-circle-right form-icon" aria-hidden="true"></i></Button>
+        <Button type="submit"><i className="fa fa-arrow-circle-right form-icon" aria-hidden="true"></i></Button>
         <Button onClick={() => this.show()}><i className="fa fa-times-circle form-icon" aria-hidden="true"></i></Button>
       </Form>)
+  }
+
+  // Function that updates state to reflect user input
+  handleFieldChange = e => {
+    const stateToChange = {}
+    stateToChange[e.target.id] = e.target.value
+    console.log(stateToChange)
+    this.setState(stateToChange)
+  }
+
+  submitRegistration = (e) => {
+    e.preventDefault()
+    let obj = {
+      email: this.state.email,
+      password: this.state.password,
+      firstName: this.state.firstName
+    }
+    //validate, submit, closes form
+    validate.newUser(obj)
+    this.show()
   }
 
   render() {
     return (
       <div>
+        {/* Register button. Shows form on click */}
         <Button size="xl" onClick={() => this.show()}><i className="fa fa-user-plus" aria-hidden="true"></i></Button>
+        {/* Checks state and shows/hides Register Form */}
         {this.state.form && <this.RegisterForm />}
       </div>
 
