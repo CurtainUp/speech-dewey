d48998cca6fd6fe151e138c0bb860b7dcac48897
Elyse Dawson <dawson.elyse@gmail.com>
2018-12-10 16:08:47 -0600

    Quiz ends with popup of quiz score and percentage. Refactored to pass state down to Quiz Score

--- src/components/quiz/QuizScore.js
+++ src/components/quiz/QuizScore.js
@@ -8,11 +8,8 @@ class QuizScore extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      correct: 0,
-      incorrect: 0,
-      skipped: 0,
       percentCorrect: 0,
-      modal: false
+      modal: true
     };
 
     this.toggle = this.toggle.bind(this);
@@ -25,56 +22,26 @@ class QuizScore extends React.Component {
   }
 
 
-
-  getAnswerScore = () => {
-    let answers = {
-      correct: 0,
-      incorrect: 0,
-      skipped: 0
-    }
-
-    let currentUser = UserSession.getUser()
-    // Fetches last 10 cardScores from database
-    API.getQuizResponses(currentUser)
-      .then(response => response.map((score) => {
-        if (score.status === "correct") {
-          answers.correct += 1
-        } else if (score.status === "incorrect") {
-          answers.incorrect += 1
-        } else if (score.status === "skipped") {
-          answers.skipped += 1
-        }
-        // returns final tabulation of each answer type.
-        return answers
-      }))
-      // Set state with answer totals
-      .then(() => this.setState({ correct: answers.correct, incorrect: answers.incorrect, skipped: answers.skipped }))
-      .then(() => this.getPercentageCorrect())
-  }
-
-
   getPercentageCorrect = () => {
-    let percentCorrect = (this.state.correct / (10 - this.state.skipped)) * 100
+    let percentCorrect = (this.props.state.correct / (10 - this.props.state.skipped)) * 100
     percentCorrect = percentCorrect.toString() + "%"
     this.setState({percentCorrect: percentCorrect})
   }
 
   componentDidMount() {
-    this.getAnswerScore()
+    this.getPercentageCorrect()
   }
 
   render() {
     return (
       <div>
-        <Button className="card-button" onClick={this.toggle}><i className="fas fa-trash-alt form-icon"></i><h3>Score</h3></Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}><h2>Your Score:</h2></ModalHeader>
           <ModalBody>
-            <h4>*category*</h4>
             <p className="modal-p">{this.state.percentCorrect}</p>
-            <h3>Correct: {this.state.correct}</h3>
-            <h3>Incorrect: {this.state.incorrect}</h3>
-            <h3>Skipped: {this.state.skipped}</h3>
+            <h3>Correct: {this.props.state.correct}</h3>
+            <h3>Incorrect: {this.props.state.incorrect}</h3>
+            <h3>Skipped: {this.props.state.skipped}</h3>
           </ModalBody>
           <ModalFooter>
             <Button color="success" className="modal-button"><i className="fas fa-check form-icon"></i><h3>Retry</h3></Button>{' '}
