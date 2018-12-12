2dcb458da0ad154f3d9df514ece6f42f5a7d4000
Elyse Dawson <dawson.elyse@gmail.com>
2018-12-12 15:59:35 -0600

    Logout button now clears session storage and returns user to landing page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,8 @@ import {
   NavItem,
   NavLink,
 } from 'reactstrap';
+import UserSession from '../modules/User/UserSession'
+import { Link } from 'react-router-dom'
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -20,6 +22,7 @@ export default class NavBar extends React.Component {
       isOpen: !this.state.isOpen
     });
   }
+
   render() {
     return (
       <div>
@@ -29,7 +32,7 @@ export default class NavBar extends React.Component {
               <NavLink href="/welcome"><i className="fas fa-home form-icon"></i></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/"><i className="fas fa-sign-out-alt form-icon"></i></NavLink>
+              <NavLink tag={Link} onClick={() => { UserSession.logOutUser() }} to="/"><i className="fas fa-sign-out-alt form-icon"></i></NavLink>
             </NavItem>
           </Nav>
         </Navbar>
